4d33e0475fc0d2b5e308f42fba9b62e70ce7000d
Akihiro Nagami <nagami@kikyos.co.jp>
2019-03-31 17:00:34 +0900

    シミュレータと実機の動作の違いでアホほどハマった。

--- CarrentLocation/ViewController.swift
+++ CarrentLocation/ViewController.swift
@@ -1,20 +1,36 @@
-//
-//  ViewController.swift
-//  CarrentLocation
-//
-//  Created by 永見彰宏 on 2019/03/30.
-//  Copyright © 2019 永見彰宏. All rights reserved.
-//
+import  UIKit
+import CoreLocation
 
-import UIKit
-
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupLocationManager()
     }
-
-
+    
+    func setupLocationManager() {
+        locationManager = CLLocationManager()
+        guard let locationManager = locationManager else { return }
+        locationManager.requestWhenInUseAuthorization()
+        
+        let status = CLLocationManager.authorizationStatus()
+        if status == .authorizedWhenInUse {
+            locationManager.delegate = self
+            //何メートル移動した時に更新をかけるか
+            locationManager.distanceFilter = 10
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.first
+        let latitude = location?.coordinate.latitude
+        let longitude = location?.coordinate.longitude
+        
+        print("latitude: \(latitude!)\nlongitude: \(longitude!)")
+        //シミュレータで実行する場合、メニューのDebug→location→CustomLocationに現在位置を取得する
+        //そこに設定した値がコンソールに表示されているはず
+    }
 }
-
